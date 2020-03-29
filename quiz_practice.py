642b8ce2e8158dd83f3674e1ecadddcc4786d114
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-03-29 08:39:46 -0700

    Completed through 10 with removals

--- quiz_practice.py
+++ quiz_practice.py
@@ -2,3 +2,76 @@ def check_for_name(sen, name):
 	return name.lower() in sentence.lower()
 
 
+def every_other_letter(word):
+	return word[::2]
+
+def add_exclam(word):
+	while len(word) < 20:
+		word += "!"
+	return word
+
+num_b =  random.sample(range(100,0)12)
+
+def print_emp(list):
+	for employee in list:
+		name = employee[0]
+		number = employee[1]
+
+
+def remove_mid(lst, start, end):
+	return lst[:start] + lst[end+1:]
+
+def more_than(lst, item, n):
+	if lst.count(item) > n:
+		return True
+	return False
+
+
+def combine_sort(lst1, lst2):
+	return sorted(lst1 + lst2)
+
+def every_three(start):
+	return list(range(start, 101,3))
+
+def cap_size(students):
+	students_in = []
+	while len(students) < 6:
+		students_in.pop(students)
+
+
+def fix(users):
+	new = []
+	for word in users:
+		if word[0] == "@"
+		new.append(word)
+
+	new = [word for word in words if words]
+
+[i for i in my_list if i>5]
+
+def up_to(num):
+	new_lst = []
+	for i in range(0,num,2):
+		new_list.append(i)
+
+def start_even(lst):
+	new = []
+	while lst[0] % 2 == 0:
+
+
+def odd_ind(lst):
+	new = []
+	for i in range(0, lst):
+		if i % 2 != 0:
+			new.append(i)
+
+def exponents(bases, powers):
+	new = []
+	for base in bases:
+		for power in powers:
+			new. append(base ** power)
+	return new
+
+def max_num(nums):
+	nums = sorted(nums)
+	return nums[-1]
