2c55c5781e6231f9c91c1741cb18f4eb94e041e5
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-04-02 21:43:56 -0700

    1 hour JS practice

--- js_practice.js
+++ js_practice.js
@@ -1,5 +1,76 @@
 function golf(par, strokes) {
 	if (strokes == 1) {
-		return name
+		return name;
+	}
+}
+
+function caseInSwitch(val) {
+	var answer = ""
+	switch(val) {
+		case 1:
+			answer = "alpha";
+			break;
+		case 2:
+			answer = "beta";
+			break;
+	}
+	return answer;
+}
+
+function caseInSwitch2(val) {
+	var answer = ""
+	switch(val) {
+		case 1:
+		case 2:
+		case 3:
+			answer = "Low";
+			break;
+		case 4:
+		case 5:
+		case 6:
+			answer = "High";
+			break;
+	}
+	return answer;
+}
+
+function abTest(a,b) {
+	if (a < 0 || b < 0 ) {
+		return undefined;
+	}
+	return Math.round(Math)
+}
+
+
+var OurDog = {
+	"name" : "Potato"
+	"friends": ["everything!"]
+};
+
+var myDog = {
+	"name": "Turnip"
+	"friends": []
+};
+
+myDog.bark = "woof";
+myDog['bark'] = "arf";
+
+delete myDog.bark;
+
+
+var lookup = {
+	"alpha" : "Adams",
+	"bravo" : "Boston",
+};
+
+result = lookup[val]
+
+var myObj = {
+	gift: "pony"
+}
+
+function checkObj(checkProp) {
+	if (myObj.hasPropery(checkProp)) {
+		return myObj[checkProp]
 	}
 }
