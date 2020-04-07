547a44a9e6dae9974d6ac5bbe0b2ae99b783f97a
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-04-07 13:18:13 -0700

    Commented out old challenges

--- quiz_practice.py
+++ quiz_practice.py
@@ -1,183 +1,183 @@
-def check_for_name(sen, name):
-	return name.lower() in sentence.lower()
+# def check_for_name(sen, name):
+# 	return name.lower() in sentence.lower()
 
 
-def every_other_letter(word):
-	return word[::2]
+# def every_other_letter(word):
+# 	return word[::2]
 
-def add_exclam(word):
-	while len(word) < 20:
-		word += "!"
-	return word
+# def add_exclam(word):
+# 	while len(word) < 20:
+# 		word += "!"
+# 	return word
 
-num_b =  random.sample(range(100,0)12)
+# num_b =  random.sample(range(100,0)12)
 
-def print_emp(list):
-	for employee in list:
-		name = employee[0]
-		number = employee[1]
-
-
-def remove_mid(lst, start, end):
-	return lst[:start] + lst[end+1:]
-
-def more_than(lst, item, n):
-	if lst.count(item) > n:
-		return True
-	return False
+# def print_emp(list):
+# 	for employee in list:
+# 		name = employee[0]
+# 		number = employee[1]
 
 
-def combine_sort(lst1, lst2):
-	return sorted(lst1 + lst2)
+# def remove_mid(lst, start, end):
+# 	return lst[:start] + lst[end+1:]
 
-def every_three(start):
-	return list(range(start, 101,3))
-
-def cap_size(students):
-	students_in = []
-	while len(students) < 6:
-		students_in.pop(students)
+# def more_than(lst, item, n):
+# 	if lst.count(item) > n:
+# 		return True
+# 	return False
 
 
-def fix(users):
-	new = []
-	for word in users:
-		if word[0] == "@"
-		new.append(word)
+# def combine_sort(lst1, lst2):
+# 	return sorted(lst1 + lst2)
 
-	new = [word for word in words if words]
+# def every_three(start):
+# 	return list(range(start, 101,3))
 
-[i for i in my_list if i>5]
-
-def up_to(num):
-	new_lst = []
-	for i in range(0,num,2):
-		new_list.append(i)
-
-def start_even(lst):
-	new = []
-	while lst[0] % 2 == 0:
+# def cap_size(students):
+# 	students_in = []
+# 	while len(students) < 6:
+# 		students_in.pop(students)
 
 
-def odd_ind(lst):
-	new = []
-	for i in range(0, lst):
-		if i % 2 != 0:
-			new.append(i)
+# def fix(users):
+# 	new = []
+# 	for word in users:
+# 		if word[0] == "@"
+# 		new.append(word)
 
-def exponents(bases, powers):
-	new = []
-	for base in bases:
-		for power in powers:
-			new. append(base ** power)
-	return new
+# 	new = [word for word in words if words]
 
-def max_num(nums):
-	nums = sorted(nums)
-	return nums[-1]
+# [i for i in my_list if i>5]
 
-def reverse(lst1, lst2):
-	for index in range(len(lst1)):
-		if lst1[index] != lst2
+# def up_to(num):
+# 	new_lst = []
+# 	for i in range(0,num,2):
+# 		new_list.append(i)
+
+# def start_even(lst):
+# 	new = []
+# 	while lst[0] % 2 == 0:
 
 
+# def odd_ind(lst):
+# 	new = []
+# 	for i in range(0, lst):
+# 		if i % 2 != 0:
+# 			new.append(i)
 
-def letter_check(word, letter):
-	return letter in word
+# def exponents(bases, powers):
+# 	new = []
+# 	for base in bases:
+# 		for power in powers:
+# 			new. append(base ** power)
+# 	return new
 
-def common(s1, s2):
-	lst3 = []
-	for letter in s1:
-		if letter in
+# def max_num(nums):
+# 	nums = sorted(nums)
+# 	return nums[-1]
 
-
-def uniques(word):
-	dict = {}
-	count = 0
-	for char in word:
-		dict[word] += 1
-	if dict[word] == 1:
-		count += 1
-	return count
-
-def length(sen, x):
-	sen = sen.split()
-	for word in sen:
-		if len(word) < x:
-			return False
-	return True
-
-def name(sen, name):
-	return name in sen
+# def reverse(lst1, lst2):
+# 	for index in range(len(lst1)):
+# 		if lst1[index] != lst2
 
 
-def every(word):
-	return word[::2]
 
-def remove(arr, S):
-	new = ""
-	for char in arr:
-		if char not in S:
-			new += char
-	return new
+# def letter_check(word, letter):
+# 	return letter in word
 
-def first(word):
-	for char in word:
-		if word.count(char) == 1:
-		return char 
-
-def adj(word):
-	new = ""
-	for i in range(len(word)-1):
-		if word[i] != word[i+1]:
-			new += word[i]
-		return new
-
-def highlow(word):
-	return sorted(word[0], word[-1])
-
-def reverse(sen):
-	return sen[::-1]
-
-def hide(arr):
-	return arr[:-4] 
-
-def reverse(lst):
-	return [-x for x in lst]
-
-def iso(word):
-	for char in word:
-		if word.count(char) > 1:
-			return False
-
-def shortest(sen):
-	shortest = ""
-	sen = sen.split()
-	for word in sen:
+# def common(s1, s2):
+# 	lst3 = []
+# 	for letter in s1:
+# 		if letter in
 
 
-def vowel(word):
-	nums = []
-	for i in range(len(word)):
-		if word[i] in "aeiou":
-			nums.append(i)
-	return nums
+# def uniques(word):
+# 	dict = {}
+# 	count = 0
+# 	for char in word:
+# 		dict[word] += 1
+# 	if dict[word] == 1:
+# 		count += 1
+# 	return count
+
+# def length(sen, x):
+# 	sen = sen.split()
+# 	for word in sen:
+# 		if len(word) < x:
+# 			return False
+# 	return True
+
+# def name(sen, name):
+# 	return name in sen
 
 
-def max_prod(arr):
-	highest = 0
-	for i in range(len(arr)- 1):
-		if arr[i] * arr[i+1] > highest:
-			highest = arr[i] * arr[i+1]
-	return highest
+# def every(word):
+# 	return word[::2]
 
-def cap_evens(word):
-	new = ""
-	for i in range(len(word)):
-		if i % 2 == 0:
-			new += word[i]
-	return new
+# def remove(arr, S):
+# 	new = ""
+# 	for char in arr:
+# 		if char not in S:
+# 			new += char
+# 	return new
 
-def add_ind(arr, num):
-	for i in range(len(arr)):
+# def first(word):
+# 	for char in word:
+# 		if word.count(char) == 1:
+# 		return char 
+
+# def adj(word):
+# 	new = ""
+# 	for i in range(len(word)-1):
+# 		if word[i] != word[i+1]:
+# 			new += word[i]
+# 		return new
+
+# def highlow(word):
+# 	return sorted(word[0], word[-1])
+
+# def reverse(sen):
+# 	return sen[::-1]
+
+# def hide(arr):
+# 	return arr[:-4] 
+
+# def reverse(lst):
+# 	return [-x for x in lst]
+
+# def iso(word):
+# 	for char in word:
+# 		if word.count(char) > 1:
+# 			return False
+
+# def shortest(sen):
+# 	shortest = ""
+# 	sen = sen.split()
+# 	for word in sen:
+
+
+# def vowel(word):
+# 	nums = []
+# 	for i in range(len(word)):
+# 		if word[i] in "aeiou":
+# 			nums.append(i)
+# 	return nums
+
+
+# def max_prod(arr):
+# 	highest = 0
+# 	for i in range(len(arr)- 1):
+# 		if arr[i] * arr[i+1] > highest:
+# 			highest = arr[i] * arr[i+1]
+# 	return highest
+
+# def cap_evens(word):
+# 	new = ""
+# 	for i in range(len(word)):
+# 		if i % 2 == 0:
+# 			new += word[i]
+# 	return new
+
+# def add_ind(arr, num):
+# 	for i in range(len(arr)):
 		
