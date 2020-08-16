6b38fd6a59e91460b12699dd98c5c0f04ee5dd43
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-08-16 11:48:14 -0700

    Revamped code challenges folder

--- def has_balanced_parens(phrase).py
+++
@@ -1,9 +0,0 @@
-def has_balanced_parens(phrase):
-    """Does a string have balanced parentheses?"""
-    counter = 0
-    for char in phrase:
-        if char == "(":
-            counter += 1
-        if char == ")":
-            counter -= 1
-    return True if counter == 0 else False
