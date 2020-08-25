2b1f2fae17ff2ab01726842b7d6b939afdeb2d03
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-08-25 16:37:24 -0700

    Retried prefix

--- Python/common_pre.py
+++ Python/common_pre.py
@@ -1,11 +1,11 @@
 
 def longestCommonPrefix(arr):
-    
-    arr.sort()
     result = ""
-    for x, y in zip(arr[0], arr[-1]):
-        if x == y: p+=x
-        else: break
+    for x,y in zip(arr[0], arr[-1]):
+        if x == y:
+            result += x
+        else:
+            break
     return result
 
-print(longestCommonPrefix(["pie", "pig"]))
+print(longestCommonPrefix(["pie", "pig", "pit"]))
