398fedbb3470597016b25b65c39078da6c6e8f43
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-08-24 17:41:21 -0700

    Merge challenge

---
+++ Python/merge.py
@@ -0,0 +1,19 @@
+
+def sort_ab(a, b):
+	out = []
+	ia = 0 # index into list a
+	ib = 0 # index into list b
+	while ia < len(a) and ib < len(b):
+# Check current items in both lists:
+# - if a < b, add a and increase ia
+# - else add b and increase ib
+	if a[ia] < b[ib]:
+		out.append(a[ia])
+		ia += 1
+	else:
+		out.append(b[ib])
+		ib += 1
+	# One list could have things remaining; add any remaining items
+	out.extend(a[ia:])
+	out.extend(b[ib:])
+	return out
