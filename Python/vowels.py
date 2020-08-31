47e5f83b608d7b3a1ae4b4b6a4d8e1f6c314ec67
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-08-30 19:53:28 -0700

    Added additional solution for vowels:

--- Python/vowels.py
+++ Python/vowels.py
@@ -5,3 +5,13 @@ def vowels(str):
 		if str[i] and str[i+1] and str[i+2] in vowels:
 			count += 1
 	return count
+
+
+import re
+def threevowels(string):
+	arr = string.split('')
+	count = 0
+	for word in arr:
+		if re.search(r'[aeious]{3,}', word) != None
+		count += 1
+	return count
